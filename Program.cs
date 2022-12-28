6ee06eebebba67e79e37ccf25561662f24f4b744
margarita <volnyakovamsr92@gmail.com>
2022-12-28 10:25:07 +0700

    задача №3

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@
  {
      Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
  }
- */
+ 
  //Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
  System.Console.Write("Введите коррдинату x точки А: ");
@@ -37,3 +37,14 @@
 
  double result = Math.Round(Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1-y2),2) + Math.Pow((z1-z2),2)),2); 
  System.Console.WriteLine($"Расстояние между тремя точками: {result}");
+*/
+
+//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Введи число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= x; i++)
+{
+System.Console.Write(Math.Pow(i, 3)+ " ");
+}
